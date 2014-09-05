02f86c1ae6f83ea1f58b6eb555fd93a7f3f58e79
Mark <mbagget1@utk.edu>
2014-09-05 17:09:18 -0400

    Adding Splash Page Files

---
+++ libraries/utk/mark.js
@@ -0,0 +1,4 @@
+/* Theme Stuff */
+jQuery(document).ready(function() {
+	jQuery("head").append("<link rel='stylesheet' type='text/css' href='../collections/sites/all/libraries/utk/splashstyle.css'>");
+});
