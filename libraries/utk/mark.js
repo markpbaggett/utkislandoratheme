300643efa3ba3fd090296b45b04b74800e695f76
Mark <mbagget1@utk.edu>
2014-09-15 17:21:58 -0400

    Fixed Dropdown List for Splash Page

--- libraries/utk/mark.js
+++ libraries/utk/mark.js
@@ -1,4 +1,6 @@
 /* Theme Stuff */
 jQuery(document).ready(function() {
 	jQuery("head").append("<link rel='stylesheet' type='text/css' href='../collections/sites/all/libraries/utk/splashstyle.css'>");
+	jQuery(("option[value='gsmrc:derris']")).prependTo("#edit-collection-select");
+	jQuery(("option[value='gsmrc:derris']")).attr('selected','selected');
 });
