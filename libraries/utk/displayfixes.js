552f825785eeeeb060e6640caf936951b350ed52
Mark <mbagget1@utk.edu>
2014-09-17 09:42:31 -0400

    Added New CSS to Handle Non Splash Pages and Refactored exising JS and CSS pages to be able to handle search boxes

--- libraries/utk/displayfixes.js
+++ libraries/utk/displayfixes.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function() {
+	/* Add Overarching CSS */
+	jQuery("head").append("<link rel='stylesheet' type='text/css' href='http://digital.lib.utk.edu/collections/sites/all/libraries/utk/overarching.css'>");
 	/* Fix PIDs in Breadcrumbs */
 	jQuery(("a[value='gsmrc:derris']")).text('The William Derris Slide Collection');
 });
--- libraries/utk/mark.js
+++ libraries/utk/mark.js
@@ -1,6 +1,9 @@
 /* Theme Stuff */
 jQuery(document).ready(function() {
-	jQuery("head").append("<link rel='stylesheet' type='text/css' href='../collections/sites/all/libraries/utk/splashstyle.css'>");
-	jQuery(("option[value='gsmrc:derris']")).prependTo("#edit-collection-select");
-	jQuery(("option[value='gsmrc:derris']")).attr('selected','selected');
+	jQuery("head").append("<link rel='stylesheet' type='text/css' href='http://digital.lib.utk.edu/collections/sites/all/libraries/utk/splashstyle.css'>");
+	var collectionname = jQuery("#splashintrotext").attr('href');
+	if ( collectionname == 'http://digital.lib.utk.edu/collections/derriscollection'){
+		jQuery(("option[value='gsmrc:derris']")).prependTo("#edit-collection-select");
+		jQuery(("option[value='gsmrc:derris']")).attr('selected','selected');
+	}
 });
