e0ea60c0a6f9eecb08751f0f14e205b320d240b0
Mark <mbagget1@utk.edu>
2014-09-17 11:01:36 -0400

    Added a Loop to replace breadcrumb pids so that it caught more than the last entry

--- libraries/utk/displayfixes.js
+++ libraries/utk/displayfixes.js
@@ -1,13 +1,16 @@
 jQuery(document).ready(function() {
 	/* Add Overarching CSS */
 	jQuery("head").append("<link rel='stylesheet' type='text/css' href='http://digital.lib.utk.edu/collections/sites/all/libraries/utk/overarching.css'>");
+	
 	/* Fix PIDs in Breadcrumbs */
 	jQuery(".breadcrumb > a").attr('class','breadcrumbanchor');
-	var breadcrumbname = jQuery(".breadcrumbanchor").last().html();
-	if ( breadcrumbname == "gsmrc:derris"){
-		jQuery(".breadcrumbanchor").last().html('The William Derris Slide Collection');
-	}
-	if ( breadcrumbname == "digital:collections"){
-		jQuery(".breadcrumbanchor").last().html('Digital Collections');
-	}
+	jQuery(".breadcrumbanchor").each(function(i) {
+	/* alert(jQuery(this).html()); */
+		if ( jQuery(this).html() == "digital:collections"){
+			jQuery(this).html("Digital Collections");
+		} 
+		else if (jQuery(this).html() == "gsmrc:derris"){
+			jQuery(this).html("The William Derris Slide Collection");
+		}
+	});
 });
