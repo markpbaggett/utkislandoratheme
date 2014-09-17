98b9ec4dc4bce234fddbf63a86d7a6a55040acd9
Mark <mbagget1@utk.edu>
2014-09-17 10:32:06 -0400

    Modified displayfixes.js to Fix the display of breadcrumbs in search

--- libraries/utk/displayfixes.js
+++ libraries/utk/displayfixes.js
@@ -2,5 +2,12 @@ jQuery(document).ready(function() {
 	/* Add Overarching CSS */
 	jQuery("head").append("<link rel='stylesheet' type='text/css' href='http://digital.lib.utk.edu/collections/sites/all/libraries/utk/overarching.css'>");
 	/* Fix PIDs in Breadcrumbs */
-	jQuery(("a[value='gsmrc:derris']")).text('The William Derris Slide Collection');
+	jQuery(".breadcrumb > a").attr('class','breadcrumbanchor');
+	var breadcrumbname = jQuery(".breadcrumbanchor").last().html();
+	if ( breadcrumbname == "gsmrc:derris"){
+		jQuery(".breadcrumbanchor").last().html('The William Derris Slide Collection');
+	}
+	if ( breadcrumbname == "digital:collections"){
+		jQuery(".breadcrumbanchor").last().html('Digital Collections');
+	}
 });
