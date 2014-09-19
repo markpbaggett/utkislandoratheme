afe07f586ae12f87a6a61138f34ca7895dd2944b
Mark <mbagget1@utk.edu>
2014-09-19 10:57:53 -0400

    Added Comment

--- libraries/utk/mark.js
+++ libraries/utk/mark.js
@@ -2,6 +2,8 @@
 jQuery(document).ready(function() {
 	jQuery("head").append("<link rel='stylesheet' type='text/css' href='http://digital.lib.utk.edu/collections/sites/all/libraries/utk/splashstyle.css'>");
 	var collectionname = jQuery("#splashintrotext").attr('href');
+	
+	/* Collection Dropdown Selector */
 	if ( collectionname == 'http://digital.lib.utk.edu/collections/derriscollection'){
 		jQuery(("option[value='gsmrc:derris']")).prependTo("#edit-collection-select");
 		jQuery(("option[value='gsmrc:derris']")).attr('selected','selected');
