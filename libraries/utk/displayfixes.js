c36dab56a5681214c500f3bc0dad622a58dc2e1c
Mark <mbagget1@utk.edu>
2014-11-14 13:46:44 -0500

    Made Page-Titles Appear except on Splash Pages

--- libraries/utk/displayfixes.js
+++ libraries/utk/displayfixes.js
@@ -26,6 +26,15 @@ jQuery(document).ready(function() {
 		}	
 	});
 	
+		/* Remove Collection Titles from Splash Pages*/
+	var pagetitle = jQuery("#page-title").html();
+		if ((pagetitle) == "derriscollection"){
+			jQuery("#page-title").remove();
+		}
+		else if ((pagetitle) == "danielcartooncollection"){
+			jQuery("#page-title").remove();
+		}
+		
 	/* Fix Names in Dropdown List */
 	jQuery(("option[value='gsmrc:derris']")).html("William Derris Slide Collection");
 	jQuery(("option[value='gsmrc:adams']")).html("Paul J. Adams Photograph Collection");
